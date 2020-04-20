497024d14a4034263d3e01aab48b66e2ae438cbf
ed9ar <a01339939@itesm.mx>
2020-04-19 20:52:23 -0500

    Storyboard
    storyboard

---
+++ TablasFiltradas-3/Tablas201711/MainViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MainViewController.swift
+//  Tablas201711
+//
+//  Created by Edgar Lopez Valdes on 19/04/20.
+//  Copyright © 2020 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    @IBOutlet weak var RegistrateBtn: UIButton!
+    @IBOutlet weak var LoginLabel: UILabel!
+    @IBOutlet weak var Login: UIButton!
+    @IBOutlet weak var Ingresa: UIButton!
+    @IBOutlet weak var contrasenia: UITextField!
+    @IBOutlet weak var usuario: UITextField!
+    
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    
+    @IBAction func RegistrateAction(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+
---
+++ TablasFiltradas-3/Tablas201711/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Tablas201711
+//
+//  Created by Edgar Lopez Valdes on 19/04/20.
+//  Copyright © 2020 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+}
+
+
