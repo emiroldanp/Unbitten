044489b8fc6b6e9dc06723e09f1ab2d7994418ff
EmilianoR <emiroldanp@hotmail.com>
2020-04-21 20:37:31 -0500

    Avances segues

---
+++ TablasFiltradas-3/Tablas201711/LiveViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LiveViewController.swift
+//  Tablas201711
+//
+//  Created by Emiliano Roldan on 21/04/20.
+//  Copyright © 2020 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class LiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
