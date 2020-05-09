fc012007266a8116e1b6ac7c14c36f53086a8b79
EmilianoR <emiroldanp@hotmail.com>
2020-05-09 14:53:57 -0500

    Change pic view
    Added the view
    Connected it to tha nav controller
    Added the image view
    Added the button for changing views with its segue
    Fixed some constraints

---
+++ TablasFiltradas-3/Tablas201711/ChangePicViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChangePicViewController.swift
+//  Tablas201711
+//
+//  Created by Emiliano Roldan on 09/05/20.
+//  Copyright © 2020 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class ChangePicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
