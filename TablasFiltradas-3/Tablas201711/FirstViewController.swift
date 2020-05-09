54815273ffd621df65fe4690a0577affcd871875
Alan Mendoza <alan.mendoza096@gmail.com>
2020-05-09 14:46:42 -0400

    Mapas
    Se cambio el FirstViewController para mostrar un mapa con los pines reales

--- TablasFiltradas-3/Tablas201711/FirstViewController.swift
+++ TablasFiltradas-3/Tablas201711/FirstViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController,CLLocationManagerDelegate{
+    @IBOutlet weak var mapa: MKMapView!
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createAnnotations(locations: annotationLocations)
+    }
+    let annotationLocations = [
+        ["title":"Krispy Kreme", "latitude": 19.3056727, "longitude": -99.1659538],
+        ["title":"Tacos Charly", "latitude": 19.2933369, "longitude": -99.16996],
+        ["title":"Sushi Roll", "latitude": 19.3054303, "longitude": -99.2025131]
+    ]
+    
+    func createAnnotations(locations: [[String : Any]]){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    locationManager.requestWhenInUseAuthorization()
+        mapa.mapType=MKMapType.standard
+        for location in locations{
+            let annotations = MKPointAnnotation()
+            annotations.title = location["title"] as? String
+            annotations.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! CLLocationDegrees, longitude: location["longitude"] as! CLLocationDegrees)
+            
+             mapa.addAnnotation(annotations)
+        }
+      
+        //let span=MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
+        //mapa.region=MKCoordinateRegion(center: cl, span: span)
+       
+        //mapa.addAnnotation(rest1)
+        mapa.showsCompass=true
+        mapa.showsScale=true
+        mapa.showsTraffic=true
+        mapa.isZoomEnabled=true
+        
+        
     }
 
+    func locationManager(_ manager: CLLocationManager,
+                         didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse{
+            locationManager.startUpdatingLocation()
+            mapa.showsUserLocation = true
+        } else {
+            locationManager.stopUpdatingLocation()
+            mapa.showsUserLocation = false
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
