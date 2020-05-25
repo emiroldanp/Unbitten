392726ef6adfa3d9449e07a4f464007066dca2aa
ed9ar <a01339939@itesm.mx>
2020-05-24 20:30:30 -0500

    Fire
    fire

--- TablasFiltradas-3/Tablas201711/CompraViewController.swift
+++ TablasFiltradas-3/Tablas201711/CompraViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
 
 class CompraViewController: UIViewController {
     var handle: AuthStateDidChangeListenerHandle?
@@ -35,12 +36,58 @@ class CompraViewController: UIViewController {
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    @IBAction func confirmar(_ sender: Any) {
+        let user = Auth.auth().currentUser
+        if let user = user {
+                // The user's ID, unique to the Firebase project.
+                // Do NOT use this value to authenticate with your backend server,
+                // if you have one. Use getTokenWithCompletion:completion: instead.
+                let uid = user.uid
+                let nom:String = uid+"ultima.png"
+            let storageRef = Storage.storage().reference().child(nom)
+            if(image.image != nil){
+                let compressedImage = image.image!
+                if let uploadData = compressedImage.pngData(){
+                    storageRef.putData(uploadData, metadata: nil, completion: {(metadata,error) in //self.hideActivityIndicator(view:self.view)
+                        if error != nil{
+                            //self.writeDatabaseCustomer()
+                            print("error")
+                            return
+                        }else{
+                            let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                            storageRef.downloadURL(completion: {(url, error) in
+                                print("Image URL: \((url?.absoluteString)!)")
+                                changeRequest?.displayName = self.Promo.text
+                                changeRequest?.commitChanges { (error) in
+                                  // ...
+                                }
+                                //self.writeDatabaseCustomer(imageUrl: (url?.absoluteString)!)
+                            })
+                            
+                            
+                            
+                        }
+                    })
+                }
+            }
+        }
+        //SecondViewController.load()
+        /*let vc = self.storyboard?.instantiateViewController(withIdentifier: "pedid")
+        
+        vc!.modalPresentationStyle = .fullScreen
+        self.present(vc!, animated: true, completion: nil)*/
+        
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         if segue.identifier == "Confirmar" {
+            
             let siguienteVista = segue.destination as! SecondViewController
             siguienteVista.image = imagen
             siguienteVista.promo = promo;
+        
+            //siguienteVista.si = 1;
             //firebase.analytics().setUserProperties({Ultima: 'apples'});
             //siguienteVista.imagen = laImagen.image
             
--- TablasFiltradas-3/Tablas201711/SecondViewController.swift
+++ TablasFiltradas-3/Tablas201711/SecondViewController.swift
@@ -8,28 +8,58 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseStorage
 
 class SecondViewController: UIViewController {
     var handle: AuthStateDidChangeListenerHandle?
     var image:UIImage!
     var promo:String!
+    
     @IBOutlet weak var Promo: UILabel!
     @IBOutlet weak var imagen: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         imagen.image = image
         Promo.text = promo
-        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
-          // ...
+        let user = Auth.auth().currentUser
+        
+        if let user = user {
+         // The user's ID, unique to the Firebase project.
+         // Do NOT use this value to authenticate with your backend server,
+         // if you have one. Use getTokenWithCompletion:completion: instead.
+         let uid = user.uid
+         let email = user.email
+         let photoUrl = user.photoURL;
+            let display = user.displayName
+        print(email)
+        print(photoUrl)
+        print("IHIUHDIUH")
+            
+        //correo.text = email
+        //let nom:String = uid+".png"
+        let nom:String = uid+"ultima.png"
+        let storageRef = Storage.storage().reference().child(nom)
+        
+        // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
+        storageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+          if let error = error {
+            // Uh-oh, an error occurred!
+          } else {
+            // Data for "images/island.jpg" is returned
+            let image = UIImage(data: data!)
+            self.imagen.image = image
+          }
         }
-        // Do any additional setup after loading the view, typically from a nib.
+        if(display != nil){
+            Promo.text = display
+        }// Do any additional setup after loading the view, typically from a nib.
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
