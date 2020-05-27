5780872dd193b896e3df65e8a908f38d62c32a64
ed9ar <a01339939@itesm.mx>
2020-05-26 23:33:14 -0500

    Listo
    listo

--- TablasFiltradas-3/Tablas201711/CompraViewController.swift
+++ TablasFiltradas-3/Tablas201711/CompraViewController.swift
@@ -38,58 +38,62 @@ class CompraViewController: UIViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     @IBAction func confirmar(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "pedid")
+        vc!.modalPresentationStyle = .fullScreen
         
         let user = Auth.auth().currentUser
-        if let user = user {
-                // The user's ID, unique to the Firebase project.
-                // Do NOT use this value to authenticate with your backend server,
-                // if you have one. Use getTokenWithCompletion:completion: instead.
-                let uid = user.uid
-                let nom:String = uid+"ultima.png"
-            let storageRef = Storage.storage().reference().child(nom)
-            if(image.image != nil){
-                let compressedImage = image.image!
-                if let uploadData = compressedImage.pngData(){
-                    storageRef.putData(uploadData, metadata: nil, completion: {(metadata,error) in //self.hideActivityIndicator(view:self.view)
-                        if error != nil{
-                            //self.writeDatabaseCustomer()
-                            print("error")
-                            return
-                        }else{
-                            let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
-                            storageRef.downloadURL(completion: {(url, error) in
-                                print("Image URL: \((url?.absoluteString)!)")
-                                changeRequest?.displayName = self.Promo.text
-                                changeRequest?.commitChanges { (error) in
-                                  // ...
-                                }
-                                //self.writeDatabaseCustomer(imageUrl: (url?.absoluteString)!)
-                            })
-                        }
-                    })
-                }
-            }
+                   if let user = user {
+                           // The user's ID, unique to the Firebase project.
+                           // Do NOT use this value to authenticate with your backend server,
+                           // if you have one. Use getTokenWithCompletion:completion: instead.
+                           let uid = user.uid
+                           let nom:String = uid+"ultima.png"
+                       let storageRef = Storage.storage().reference().child(nom)
+                       if(self.image.image != nil){
+                           let compressedImage = self.image.image!
+                           if let uploadData = compressedImage.pngData(){
+                               storageRef.putData(uploadData, metadata: nil, completion: {(metadata,error) in //self.hideActivityIndicator(view:self.view)
+                                   if error != nil{
+                                       //self.writeDatabaseCustomer()
+                                       print("error")
+                                       return
+                                   }else{
+                                       let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                                       storageRef.downloadURL(completion: {(url, error) in
+                                           print("Image URL: \((url?.absoluteString)!)")
+                                           changeRequest?.displayName = self.Promo.text
+                                           changeRequest?.commitChanges { (error) in
+                                             // ...
+                                           }
+                                           //self.writeDatabaseCustomer(imageUrl: (url?.absoluteString)!)
+                                       })
+                                   }
+                               })
+                           }
+                       }
+                   }
+        let context = LAContext()
+        var error: NSError?
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return print(error)
         }
-        
-        let context:LAContext = LAContext()
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) {
-            context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "¿Desea confirmar el pedido?") { (good, error) in
-                if good {
-                    print("GOOD")
-                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "CompraV")
-                    //vc!.modalPresentationStyle = .fullScreen
-                    //self.present(vc!, animated: true, completion: nil)
-                    
-                } else {
-                    print("TRY AGAIN")
-                    
-                }
+
+        let reason = "Face ID "
+        context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { isAuthorized, error in
+            guard isAuthorized == true else {
+                return print(error)
             }
+            
+            //self.performSegue(withIdentifier: "comp", sender: self.navigationController)
+            DispatchQueue.main.async {
+                self.present(vc!, animated: true, completion: nil)
+                //self.imageView.image = image
+            }
+            print("success")
+           
+            //self.present(vc!, animated: true, completion: nil)
         }
-        
-        
-        
+            
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
